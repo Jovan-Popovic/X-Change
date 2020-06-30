7e8ea2818fc435112c65b0d48266e2f1c262f55a
Alan432 <alan.adzagic@gmail.com>
2020-06-30 16:58:06 +0200

    profile responsive

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,67 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 import profile from "../img/profile.png";
-import phone from "../img/phone.png";
-import review from "../img/review.png";
 import settings from "../img/settings.png";
-import addnew from "../img/add-new.png";
-import { Drawer } from "antd";
 
-const DrawerSettings = () => {
-  const [visible, setVisible] = useState(false);
+export class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div id="quickviewDefault" className="quickview">
+          <header className="quickview-header">
+            <p className="title">Quickview title</p>
+            <span className="delete" data-dismiss="quickview"></span>
+          </header>
 
-  const toggleDrawer = () => setVisible(!visible);
+          <div className="quickview-body">
+            <div className="quickview-block">
+              ...
+            </div>
+          </div>
+          <footer className="quickview-footer">
 
-  return (
-    <React.Fragment>
-      <button className="settings-btn" type="primary" onClick={toggleDrawer}>
-        <img src={settings} alt="" className="profile-sett" />
-      </button>
-      <Drawer
-        title="Settings:"
-        placement="right"
-        closable={false}
-        onClose={toggleDrawer}
-        visible={visible}
-      >
-        <div className="drawer-div">
-          <a href>Help</a>
-          <a href>About Developers</a>
-          <a href>Edit Profile</a>
-          <a href>Delete Account</a>
+          </footer>
         </div>
-      </Drawer>
-    </React.Fragment>
-  );
-};
-
-export const Profile = () => {
-  return (
-    <div>
-      <div className="profile-stats">
-        <div>
-          <img src={profile} alt="" className="profile-pic" />
-        </div>
-        <div className="stats-text">
-          <strong>Username</strong>
-          <p>Email</p>
-          <p>
-            <img src={phone} alt="" /> Phone
-          </p>
-          <a href>
-            <img src={review} alt="" />
-            Profile rating
-          </a>
-        </div>
-        <div className="profile-end">
-          <DrawerSettings />
+        <div className="columns is-variable is-1-mobile is-1-tablet is-1-desktop is-1-widescreen is-1-fullhd">
+          <div className="column is-one-third" id="picture">
+            <figure className="image is-1by1">
+              <img src={profile} className="is-rounded" alt="" />
+            </figure>
+          </div>
+          <div className="column">
+            <strong className="is-size-3-mobile is-size-2-tablet is-size-1-desktop">
+              Username
+            </strong>
+            <p className="is-size-4-mobile is-size-4-tablet is-size-3-desktop">
+              First Name, Last Name
+            </p>
+            <p className="is-size-4-mobile is-size-4-tablet is-size-3-desktop">
+              Email
+            </p>
+          </div>
+          <div className="column is-1">
+            <button data-show="quickview" data-target="quickviewDefault">
+              <img src={settings} style={{width:"55px"}} alt="" />
+            </button>
+          </div>
         </div>
       </div>
-      <div className="for-add-new">
-        <a href className="add-new">
-          <img src={addnew} alt="" />
-        </a>
-      </div>
-      <hr />
-    </div>
-  );
-};
+    );
+  }
+}
